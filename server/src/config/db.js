a467f7d5169c2218884f3d666be28129168662fa
Rehan Alam <alamrehan12345678@gmail.com>
2025-05-22 22:15:37 +0545

    env updated

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,7 +3,7 @@ const config = require("./index");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(config.mongodbUri);
+    const conn = await mongoose.connect("mongodb+srv://alamrehan12345678:alamrehan12345678@cluster0.6j0sjn2.mongodb.net/civicpulse?retryWrites=true&w=majority&appName=Cluster0");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
--- server/src/config/index.js
+++ server/src/config/index.js
@@ -6,7 +6,7 @@ dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 module.exports = {
   port: process.env.PORT || 5000,
-  mongodbUri: process.env.MONGODB_URI || "mongodb://localhost:27017/civicpulse",
+  mongodbUri: process.env.MONGODB_URI || "mongodb+srv://alamrehan12345678:alamrehan12345678@cluster0.6j0sjn2.mongodb.net/civicpulse?retryWrites=true&w=majority&appName=Cluster0",
   jwtSecret: process.env.JWT_SECRET || "civicpulse_jwt_secret",
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || "7d",
 };
